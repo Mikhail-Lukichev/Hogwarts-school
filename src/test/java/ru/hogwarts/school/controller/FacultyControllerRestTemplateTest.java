525cbd52de9db26e0ef4cc30dca09c5e627b8f3b
Mikhail Lukichev <dextrumm@gmail.com>
2023-12-11 18:07:41 -0700

    WIP

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerRestTemplateTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerRestTemplateTest.java
@@ -27,56 +27,43 @@ class FacultyControllerRestTemplateTest {
 
     @Test
     void getAll() {
-//        //Data preparation
-//        Faculty newFaculty1 = new Faculty();
-//        newFaculty1.setId(1L);
-//        newFaculty1.setName("name");
-//        newFaculty1.setColor("color");
-//
-////        Faculty newFaculty2 = new Faculty();
-////        newFaculty2.setId(2L);
-////        newFaculty2.setName("name");
-////        newFaculty2.setColor("color");
-//        Faculty postedFaculty1 = this.restTemplate.postForObject("http://localhost:" + port + "/faculty", newFaculty1, Faculty.class);
-////        this.restTemplate.postForObject("http://localhost:" + port + "/faculty", newFaculty2, Faculty.class);
-//
-//
-//
-//        //Expected result preparation
-////        Collection<Faculty> expectedResult = List.of(newFaculty1, newFaculty2);
-////        Faculty expectedResult = newFaculty1;
-////        Faculty expectedResult = new Faculty();
-////        expectedResult.setId(1L);
-////        expectedResult.setName("name");
-////        expectedResult.setColor("color");
-//
-//        //Test execution
-//        Faculty actualResult = this.restTemplate.getForObject("http://localhost:" + port + "/faculty",Faculty.class);
-//        assertNotNull(actualResult);
-////        assertEquals(expectedResult,actualResult);
-    }
-
-    @Test
-    void get() {
         //Data preparation
         Faculty newFaculty1 = new Faculty();
         newFaculty1.setId(1L);
         newFaculty1.setName("name");
         newFaculty1.setColor("color");
 
+        Faculty newFaculty2 = new Faculty();
+        newFaculty2.setId(1L);
+        newFaculty2.setName("name");
+        newFaculty2.setColor("color");
+
         //Expected result preparation
         Faculty postedFaculty1 = this.restTemplate.postForObject("http://localhost:" + port + "/faculty", newFaculty1, Faculty.class);
-        Long id = postedFaculty1.getId();
+        Faculty postedFaculty2 = this.restTemplate.postForObject("http://localhost:" + port + "/faculty", newFaculty2, Faculty.class);
+//        Long id = postedFaculty.getId();
 
         //Test execution
-//        ResponseEntity<Faculty> actualResult = this.restTemplate.getForEntity("http://localhost:" + port + "/faculty?id=" + id, Faculty.class);
-//        this.restTemplate.getForEntity("http://localhost:" + port + "/faculty?id=" + id, Faculty.class);
-//        Faculty actualResult = this.restTemplate.getForObject("http://localhost:" + port + "/faculty/" + id+"/", Faculty.class);
-        String requestUrl = "http://localhost:" + port + "/faculty/" + id+"/";
-        ResponseEntity<Faculty> actualResult = this.restTemplate.getForEntity(requestUrl, Faculty.class);
-        Faculty actualResultBody = actualResult.getBody();
-//        assertNotNull(actualResult.getBody());
-        assertEquals(postedFaculty1,actualResult);
+        Collection<Faculty> actualResult = this.restTemplate.getForObject("http://localhost:" + port + "/faculty/", Collection.class);
+        System.out.println("test");
+//        assertEquals(postedFaculty,actualResult.getBody());
+    }
+
+    @Test
+    void get() {
+        //Data preparation
+        Faculty newFaculty = new Faculty();
+        newFaculty.setId(1L);
+        newFaculty.setName("name");
+        newFaculty.setColor("color");
+
+        //Expected result preparation
+        Faculty postedFaculty = this.restTemplate.postForObject("http://localhost:" + port + "/faculty", newFaculty, Faculty.class);
+        Long id = postedFaculty.getId();
+
+        //Test execution
+        ResponseEntity<Faculty> actualResult = this.restTemplate.getForEntity("http://localhost:" + port + "/faculty/{id}", Faculty.class,id);
+        assertEquals(postedFaculty,actualResult.getBody());
     }
 
     @Test
